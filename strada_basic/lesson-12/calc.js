b6bc765af38fc0a8489f91c75e326f4ac6f2e5bd
Saba Dodashvili <sabadodashvili777@gmail.com>
2022-08-16 01:09:01 +0400

    refactoring calc script

--- strada_basic/lesson-12/calc.js
+++ strada_basic/lesson-12/calc.js
@@ -2,24 +2,30 @@ let a = prompt('enter the first number', '0');
 let b = prompt('enter the second number', '0');
 let operation = prompt('enter one of the operators: +, -, *', '+');
 
-function calc(operation, numbOne, numbTwo) {
+function validateValues(operation, numbOne, numbTwo) {
 	let checkOnSpaces = numbOne === ' ' || numbTwo === ' ';
 	let notNumber = numbOne === '' || isNaN(numbOne) || numbTwo === '' || isNaN(numbTwo);
-	let operationIsCorrect = operation === '+' || operation === '-' || operation === '*';
+	let operationIsCorrect = operation === 'add' || operation === 'multi' || operation === 'substract';
 
 	if (checkOnSpaces || notNumber) return 'for the program to work correctly, enter the numbers';
 	else if (!operationIsCorrect) return 'enter the correct operator';
-	else {
+	else return 'correct validation';
+}
+
+function calc(operation, numbOne, numbTwo) {
+	if (validateValues(operation, numbOne, numbTwo) === 'correct validation') {
 		switch (operation) {
-			case '+':
-				return +numbOne + +numbTwo;
-			case '-':
-				return +numbOne - +numbTwo;
-			case '*':
-				return +numbOne * +numbTwo;
+			case 'add':
+				return Number(numbOne) + Number(numbTwo);
+			case 'substract':
+				return Number(numbOne) - Number(numbTwo);
+			case 'multi':
+				return Number(numbOne) * Number(numbTwo);
 			default:
 				return 'unexpected error please try again';
 		}
+	} else {
+		return validateValues(operation, numbOne, numbTwo);
 	}
 }
 
