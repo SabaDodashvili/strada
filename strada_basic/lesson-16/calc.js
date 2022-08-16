28c94a53b0aca73a09b00080fa1da785ad116a21
Saba Dodashvili <sabadodashvili777@gmail.com>
2022-08-16 22:02:10 +0400

    test commit

---
+++ strada_basic/lesson-16/calc.js
@@ -0,0 +1,38 @@
+let a = prompt('enter the first number', '0');
+let b = prompt('enter the second number', '0');
+let operation = prompt('enter one of the operators: add, sub, multi', 'add');
+
+function validateValues(operation, numbOne, numbTwo) {
+	let checkOnSpaces = numbOne === ' ' || numbTwo === ' ';
+	let notNumber = (numbOne !== '0' && isNaN(numbOne / numbOne)) || (numbTwo !== '0' && isNaN(numbTwo / numbTwo));
+	let operationIsCorrect = operation === 'add' || operation === 'multi' || operation === 'sub';
+
+	if (checkOnSpaces || notNumber) return 'for the program to work correctly, enter the numbers';
+	else if (!operationIsCorrect) return 'enter the correct operator';
+	else return 'correct validation';
+}
+
+function calc(operation, numbOne, numbTwo) {
+	let operations = {
+		add: '+',
+		sub: '-',
+		multi: '*',
+	};
+
+	if (validateValues(operation, numbOne, numbTwo) === 'correct validation') {
+		switch (operations[operation]) {
+			case operations.add:
+				return Number(numbOne) + Number(numbTwo);
+			case operations.sub:
+				return Number(numbOne) - Number(numbTwo);
+			case operations.multi:
+				return Number(numbOne) * Number(numbTwo);
+			default:
+				return 'unexpected error please try again';
+		}
+	} else {
+		return validateValues(operation, numbOne, numbTwo);
+	}
+}
+
+console.log(calc(operation, a, b));
