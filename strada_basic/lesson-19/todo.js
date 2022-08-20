5437786e538ec23a6272e039ace6bcc393389d3a
Saba Dodashvili <sabadodashvili777@gmail.com>
2022-08-20 22:26:29 +0400

    redid code after code review

--- strada_basic/lesson-19/todo.js
+++ strada_basic/lesson-19/todo.js
@@ -1,56 +1,57 @@
-let todo = {
-	list: {
-		'buy products': 'To Do',
-		'find job': 'in progress',
-		'make todo List': 'Done',
-	},
-
-	statuses: {
-		status_1: 'To Do',
-		status_2: 'In Progress',
-		status_3: 'Done',
-	},
-
-	changeStatus(task, newStatus) {
-		if (this.keyExists(task) && this.checkStatus(newStatus)) this.list[task] = newStatus;
-	},
-	addTask(task, status = 'To Do') {
-		if (!this.keyExists(task) && this.checkStatus(status)) this.list[task] = status;
-	},
-	deleteTask(task) {
-		if (this.keyExists(task)) delete this.list[task];
-	},
-	showList() {
-		let toDoStr = 'ToDo:\n';
-		let inProgressStr = 'In Progress:\n';
-		let doneStr = 'Done:\n';
-
-		for (const key in this.list) {
-			if (this.list[key] === 'To Do') toDoStr += `  "${key}",\n`;
-			else if (this.list[key] === 'In Progress') inProgressStr += `  "${key}",\n`;
-			else if (this.list[key] === 'Done') doneStr += `  "${key}",\n`;
-		}
-
-		console.log(toDoStr + inProgressStr + doneStr);
-	},
-
-	keyExists(keyName) {
-		if (keyName in this.list) return true;
-	},
-
-	checkStatus(status) {
-		for (const key in this.statuses) {
-			if (status === this.statuses[key]) return true;
-		}
-		return false;
-	},
+const STATUSES = {
+	To_Do: 'To Do',
+	In_Progress: 'In Progress',
+	Done: 'Done',
 };
 
-todo.changeStatus('buy products', 'In Progress');
-todo.addTask('do PR', 'To Do');
-todo.addTask('finish strada', 'In Progress');
-todo.addTask('refactor code', 'Done');
+const list = {
+	'buy products': STATUSES.To_Do,
+	'find job': STATUSES.In_Progress,
+	'make todo List': STATUSES.Done,
+};
 
-todo.deleteTask('find job');
-todo.showList();
-console.log(todo);
+function changeStatus(task, newStatus) {
+	if (keyExists(task) && checkStatus(newStatus)) list[task] = newStatus;
+}
+
+function addTask(task, status = 'To Do') {
+	if (!keyExists(task) && checkStatus(status)) list[task] = status;
+}
+
+function deleteTask(task) {
+	if (keyExists(task)) delete list[task];
+}
+
+function showList() {
+	let toDoStr = 'ToDo:\n';
+	let inProgressStr = 'In Progress:\n';
+	let doneStr = 'Done:\n';
+
+	for (const key in list) {
+		if (list[key] === 'To Do') toDoStr += `  "${key}",\n`;
+		else if (list[key] === 'In Progress') inProgressStr += `  "${key}",\n`;
+		else if (list[key] === 'Done') doneStr += `  "${key}",\n`;
+	}
+
+	console.log(toDoStr + inProgressStr + doneStr);
+}
+
+function keyExists(keyName) {
+	if (keyName in list) return true;
+}
+
+function checkStatus(status) {
+	for (const key in STATUSES) {
+		if (status === STATUSES[key]) return true;
+	}
+	return false;
+}
+
+changeStatus('buy products', 'In Progress');
+addTask('do PR', 'To Do');
+addTask('finish strada', 'In Progress');
+addTask('refactor code', 'Done');
+
+deleteTask('find job');
+showList();
+console.log(list);
